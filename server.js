0897a66b3d591ef0c68fc864adce0971d151b6c6
THORISO <220674868@ump.ac.za>
2025-10-04 10:31:14 +0200

    Fix bcrypt import and port for Render

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🌍 Server running on port ${PORT}`));
